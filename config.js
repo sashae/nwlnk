4521f7590e7d29c3c09aa0fe4800ef64f91c1bd6
sasha e <sashae@gmail.com>
2016-04-09 08:15:52 -0400

    changed selfsigned config.

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-# basic server config
+// basic server config
 var server = 'irc.subtle.org';
 var port = '6697';
 var secure = 'true';
-var selfSigned = 'false';
+//allows for self-signed or non-CA approved certs, like letsencrypt.
+var selfSigned = 'true';
 var certExpired = 'false';
 
-# where are we going
-var userName = 'nwlnk';
-var realName = 'the shortest of links';
+// where are we going
 var nick = 'nwlnk';
+var realName = 'the shortest of links';
 var channels = ['#somewhere, #bottest'];
 
-# probably want to turn this off once running for real
+// probably want to turn this off once running for real
 var debug = 'true';
 var showErrors = 'true';
 
-# shortener config
+// shortener config
 var shortenHost = 'https://pab.st';
