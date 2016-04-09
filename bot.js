1e906e9e6567c40dbb4e9a89e7136d47c102c238
sasha e <sashae@gmail.com>
2016-04-09 08:32:34 -0400

    initial commit.

---
+++ bot.js
@@ -0,0 +1,23 @@
+// bring in setup
+var config = require('./config');
+
+// set up logging to file
+const output = fs.createWriteStream('./debug.log');
+const errorOutput = fs.createWriteStream('./error.log');
+const logger = new Console(output, errorOutput);
+
+var irc = require('irc');
+
+var bot = new irc.Client(config.server, config.nick, {
+  config.port, config.secure, config.selfSigned, config.userName, config.realName, 
+	channels: config.channels, 
+	config.debug, config.showErrors,
+});
+
+// write stdout/errors to logfile
+bot.addListener('stdout', function(message) {
+    logger.log('debug: ', message);
+});
+bot.addListener('error', function(message) {
+    logger.log('error: ', message);
+});
