0b0ccf679f9370dd30694ec3ea407ea876822312
sasha e <sashae@gmail.com>
2016-04-09 10:34:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 // basic server config
-var server = 'irc.subtle.org';
-var port = '6697';
-var secure = 'true';
-var autoConnect = 'true';
+var server = "irc.subtle.org";
+var port = "6697";
+var secure = "true";
+var autoConnect = "true";
 //allows for self-signed or non-CA approved certs, like letsencrypt.
-var selfSigned = 'true';
-var certExpired = 'false';
+var selfSigned = "true";
+var certExpired = "false";
 
 // where are we going
-var nick = 'nwlnk';
-var userName = 'nwlnk';
-var realName = 'the shortest of links';
-var channels = ['#somewhere'];
+var nick = "nwlnk";
+var userName = "nwlnk";
+var realName = "the shortest of links";
+var channels = ["#somewhere"];
 
 // probably want to turn this off once running for real
-var debug = 'true';
-var showErrors = 'true';
+var debug = "true";
+var showErrors = "true";
 
 // shortener config
 var shortenHost = 'https://pab.st';
