688c07f174682a6bf43360816248c5838be8bac3
sasha e <sashae@gmail.com>
2016-04-09 08:07:40 -0400

    initial commit for config.

---
+++ config.js
@@ -0,0 +1,19 @@
+# basic server config
+var server = 'irc.subtle.org';
+var port = '6697';
+var secure = 'true';
+var selfSigned = 'false';
+var certExpired = 'false';
+
+# where are we going
+var userName = 'nwlnk';
+var realName = 'the shortest of links';
+var nick = 'nwlnk';
+var channels = ['#somewhere, #bottest'];
+
+# probably want to turn this off once running for real
+var debug = 'true';
+var showErrors = 'true';
+
+# shortener config
+var shortenHost = 'https://pab.st';
